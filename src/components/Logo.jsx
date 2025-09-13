d5b034e32219605b9229b2fdb99d95c83a15878d
Ajeet Kumar Pandit <kumarajeet19022004@gmail.com>
2025-09-13 16:17:24 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Logo({width = '100px'}) {
+  return (
+    <div >
+  
+      <img src="/blog.png" alt=""  width="40" />
+    </div>
+  )
+}
+
+export default Logo
