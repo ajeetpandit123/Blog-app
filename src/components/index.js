ef263afe4e9fe2a939e3f3884a73f5b70b6074b8
Ajeet Kumar Pandit <kumarajeet19022004@gmail.com>
2025-09-13 17:53:59 +0530

    Fix build error for Vercel deployment

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-import Select from "./Select";
+
+import Select from "./Select"
+
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "../Container/Container";
